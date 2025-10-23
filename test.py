436daeda12a3d276a3f98795e7b174a4f6c2e962
Thông lê <thonglee2k5@gmail.com>
2025-10-23 19:42:00 +0700

    cập nhật tiến trình thực hành

--- test.py
+++ test.py
@@ -11,39 +11,35 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
-#doc file train (.csv)
-df=pd.read_csv(r"train.csv")
-#lam day cac gia tri trong 
-df['Age']=df['Age'].fillna(df['Age'].mean())
-df['Cabin']=df['Cabin'].fillna(df['Cabin'].mode()[0])
-#chuan hoa du lieu so
+df=pd.read_csv("train.csv")
+has_null = df.isnull().sum().any()
+has_nan  = df.isna().sum().any()
+n_duplicated = df.duplicated().sum()
+print(f'Tính toàn vẹn dữ liệu:')
+print(f'+ Có giá trị Null: {has_null}')
+if has_null:
+    df['Age']=df['Age'].fillna(df['Age'].mean())
+    df['Cabin']=df['Cabin'].fillna(df['Cabin'].mode()[0])  
+print(f'+ Có giá trị Nan: {has_nan}')
+if has_nan:
+    df = df.dropna()   
+print(f'+ Số dòng trùng: {n_duplicated}')
+if n_duplicated>0:
+    df = df.drop_duplicates()
 scaler=MinMaxScaler()
 df[['Age','Fare']]=scaler.fit_transform(df[['Age','Fare']])
-#encode du lieu dang chu (vi du sex(male,female)=>(0,1))
 le=LabelEncoder()
 df['Sex']=le.fit_transform(df['Sex'])
-#tao cac cot Embarked
 df=pd.get_dummies(df,columns=[])
-#chia du lieu train test
 x=df[['Pclass','Sex','Age','Fare']]
+
 y=df['Survived']
+selected_features = importances[importances > 0.05].index
+x_new = x[selected_features]
 x_train,x_test,y_train,y_test=train_test_split(
-    x,y,test_size=0.2,random_state=43
+    x_new,y,test_size=0.2,random_state=43
 ) # chia 80 train 20 test 
 model=LogisticRegression()
 model.fit(x_train,y_train)
-#du doan va danh gia
 y_pred=model.predict(x_test)
 print("Do chinh xac:",accuracy_score(y_test,y_pred))
-models = [
-    ("Logistic Regression", LogisticRegression()),
-    ("KNN", KNeighborsClassifier()),
-    ("Decision Tree", DecisionTreeClassifier()),
-    ("Random Forest", RandomForestClassifier()),
-    ("SVM", SVC()),
-    ("Naive Bayes", GaussianNB())
-]
-
-for name, model in models:
-    scores = cross_val_score(model, x, y, cv=5)  # 5-fold cross validation
-    print(name, "-> Accuracy trung bình:", scores.mean())
